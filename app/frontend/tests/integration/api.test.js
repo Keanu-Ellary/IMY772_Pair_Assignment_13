727bbc554db811640bb2e0a29eb43b5189b1436a
Keanu-Ellary <161315536+Keanu-Ellary@users.noreply.github.com>
2026-05-07 20:36:36 +0200

    amend: added more integration testing

--- app/frontend/tests/integration/api.test.js
+++ app/frontend/tests/integration/api.test.js
@@ -38,19 +38,6 @@ describe("API Integration", () => {
         expect(res.ok).toBe(true);
         expect(data.result).toBe("4");
     });
-    test('Performs Addition Unsuccessfully', async() => {
-        const res = await fetch('http://localhost:5000/calculate', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({expression: "2 + +"})
-        });
-
-        const data = await res.json();
-        
-        expect(res.ok).toBe(false);
-        expect(data.error).toBeDefined();
-        expect(data.error).toBe("Input values must be a hexadecimal")
-    });
 
     // Subtraction
     test('Performs Subtraction Successfully: Simple Inputs', async() => {
@@ -88,19 +75,6 @@ describe("API Integration", () => {
         
         expect(res.ok).toBe(true);
         expect(data.result).toBe("0");
-    });
-    test('Performs Subtraction Unsuccessfully', async() => {
-        const res = await fetch('http://localhost:5000/calculate', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({expression: "20 - 30"})
-        });
-
-        const data = await res.json();
-        
-        expect(res.ok).toBe(false);
-        expect(data.error).toBeDefined();
-        expect(data.error).toBe("Output of subtraction must not be negative")
     });
 
     // Multiplication
@@ -140,19 +114,6 @@ describe("API Integration", () => {
         expect(res.ok).toBe(true);
         expect(data.result).toBe("64");
     });
-    test('Performs Multiplication Unsuccessfully', async() => {
-        const res = await fetch('http://localhost:5000/calculate', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({expression: "FFFF * FF"})
-        });
-
-        const data = await res.json();
-        
-        expect(res.ok).toBe(false);
-        expect(data.error).toBeDefined();
-        expect(data.error).toBe("Input values must be 1 or 2 digits")
-    });
 
     // Division
     test('Performs Division Successfully: Simple Inputs', async() => {
@@ -191,7 +152,139 @@ describe("API Integration", () => {
         expect(res.ok).toBe(true);
         expect(data.result).toBe("1");
     });
-    test('Performs Division Unsuccessfully', async() => {
+
+    // Errors
+    test('Should handle invalid input', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "AF +"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input must have atleast 2 hexadecimals values and 1 operator")
+    });
+    test('Should handle empty input', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: ""})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input cannot be empty")
+    });
+    test('Should handle invalid digit amount', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "1F1F1"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input must have atleast 2 hexadecimals values and 1 operator")
+    });
+    test('Should handle negative output', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "-30 + 20"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input values must be 1 or 2 digits")
+    });
+    test('Should handle decimal places', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "1.0 + 1"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input values must be 1 or 2 digits")
+    });
+    test('Should handle invalid hex numbers', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "Z + 2"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input values must be a hexadecimal")
+    });
+    test('Performs Addition Unsuccessfully - Empty addition', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "2 + +"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input values must be a hexadecimal")
+    });
+    test('Performs Addition Unsuccessfully - Invalid addition', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "2 + "})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input must have atleast 2 hexadecimals values and 1 operator")
+    });
+    test('Performs Subtraction Unsuccessfully - Output negative', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "20 - 30"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Output of subtraction must not be negative")
+    });
+    test('Performs Multiplication Unsuccessfully', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "FFFF * FF"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Input values must be 1 or 2 digits")
+    });
+    test('Performs Division Unsuccessfully - Division by 0', async() => {
         const res = await fetch('http://localhost:5000/calculate', {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
@@ -203,5 +296,18 @@ describe("API Integration", () => {
         expect(res.ok).toBe(false);
         expect(data.error).toBeDefined();
         expect(data.error).toBe("Division by 0 is not allowed")
+    });
+    test('Performs Division Unsuccessfully - Decimal output', async() => {
+        const res = await fetch('http://localhost:5000/calculate', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({expression: "1 / 4"})
+        });
+
+        const data = await res.json();
+        
+        expect(res.ok).toBe(false);
+        expect(data.error).toBeDefined();
+        expect(data.error).toBe("Decimal results not allowed")
     });
 });
