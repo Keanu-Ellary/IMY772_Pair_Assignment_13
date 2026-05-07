6c65d1ab2dec30dfc73c9370231537534944c87c
Tia-H <tiaharripersad@gmail.com>
2026-05-07 22:10:41 +0200

    fix: calculator can allow long equations

--- app/backend/calculator.py
+++ app/backend/calculator.py
@@ -24,21 +24,24 @@ def calculate_multiple(user_input):
     try:
         if len(user_input_data) <3:
             return calculate(user_input)
+        
+        is_combined_value = False
 
         while len(user_input_data) > 1:
             expr = " ".join(user_input_data[:3])
 
-            result = calculate(expr)
+            result = calculate(expr, is_combined_value)
 
             user_input_data = [result] + user_input_data[3:]
+            is_combined_value = True
 
         return user_input_data[0]
     except Exception as e:
         raise
 
-def calculate(user_input):
+def calculate(user_input, has_combined_value):
     try:
-        value_one, operator, value_two = handle_input(user_input)
+        value_one, operator, value_two = handle_input(user_input, has_combined_value)
         match operator:
             case "+":
                 result = perform_addition(value_one, value_two)
